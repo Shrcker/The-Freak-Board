0355c950027a7cf418f4b581f618b1156f41ff67
Tania <tanner.shirkey@gmail.com>
2024-05-11 20:45:19 -0400

    User access
    User may now create and access their accounts

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const loginHandler = async (event) => {
+	event.preventDefault();
+
+	const email = docoument.getElementById("email-login").value.trim();
+	const password = document.getElementById("password-login").value.trim();
+
+	if (email && password) {
+		const response = await fetch("api/users/login", {
+			method: "POST",
+			body: JSON.stringify({ email, password }),
+			headers: { "Content-Type": "application/json" },
+		});
+
+		if (response.ok) {
+			document.location.replace("/profile");
+		} else {
+			alert(response.statusText);
+		}
+	}
+};
+
+const createAccountHandler = async (event) => {
+	event.preventDefault();
+
+	const name = document.getElementById("name-signup").value.trim();
+	const email = document.getElementById("email-signup").value.trim();
+	const password = document.getElementById("passsword-signup").value.trim();
+
+	if (name && email && password) {
+		const response = await fetch("/api/users", {
+			method: "POST",
+			body: JSON.stringify({ name, email, password }),
+			headers: { "Content-Type": "application/json" },
+		});
+
+		if (response.ok) {
+			document.location.replace("/profile");
+		} else {
+			alert(response.statusText);
+		}
+	}
+};
+
+document
+	.getElementsByClassName("login-form")
+	.addEventListener("submit", loginHandler);
+
+document
+	.getElementsByClassName("signup-form")
+	.addEventListener("submit", createAccountHandler);
